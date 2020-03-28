981fb45f915b69313f637b5ced1a86282b351395
Maia <fernandamaiadeoliveira@gmail.com>
2020-03-28 14:27:44 -0300

    omni_backend

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+ 
+const OngController = require('./controllers/OngControllers');
+const IncidentController = require('./controllers/IncidentControllers');
+const ProfileController = require('./controllers/ProfileControllers');
+const SessionController = require('./controllers/SessionControllers');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes; 
